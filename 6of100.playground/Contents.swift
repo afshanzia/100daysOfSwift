6011b8f4ea3564c92b29d46eb19c359cf6216a04
Barakah Digital <barakahdigital@Barakahs-MacBook-Pro.local>
2022-09-13 18:36:28 +0300

    closures1

---
+++ 6of100.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+var greeting = "Hello, playground"
+// 1. creating basic closures
+
+//creates a function without a name, and assigns that function to driving
+let driving = {
+    print("I'm driving in my car")
+}
+// it can be called like a regular function like
+driving()
+
+// 2. adding parameter to closure
+
+let drive = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+// differences between functions and closures is that you don’t use parameter labels when running closures. So, to call driving() now we’d write this:
+drive("London")
+
+// 3.Returning values from a closure
+let drivingWithReturn = { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+let message = drivingWithReturn("London")
+print(message)
+
+// 4. closures as parameters
+let car = {
+    print("I'm driving in my car")
+}
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travel(action: car)
+
+// 5. Trailing closure syntax
+
+func travell(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travell() {
+    print("I'm driving in my car")
+}
+travell {
+    print("I'm driving in my car")
+}
